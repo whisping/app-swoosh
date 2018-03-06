edfa74b56f4997878ff481906cce2f26bdc55f94
Nikolai Dimasiuk <nikolaidimasiuk@MacBook-Nikolai.local>
2018-03-06 16:30:00 +0300

    ibactions handling events data models

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Nikolai Dimasiuk on 06.03.18.
+//  Copyright © 2018 whisping. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague : String?
+    var selectedSkillLevel : String?
+}
